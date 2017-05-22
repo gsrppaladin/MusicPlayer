9cf38ed1f37b88d40241d89d3909edbf0d8a0a22
Sam Greenhill <samgreenhill@Sams-MBP.home>
2017-05-21 20:28:46 -0400

    Set up the UI

--- MusicPlayer/ViewController.swift
+++ MusicPlayer/ViewController.swift
@@ -10,16 +10,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var songTimeLbl: UILabel!
+    
+    @IBOutlet var songNameLbl: UILabel!
+    
+    @IBOutlet var timeSlider: UISlider!
+    
+    @IBOutlet var volumeSlider: UISlider!
+    
+    @IBOutlet var volumeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidLayoutSubviews() {
+        
+        volumeSlider.transform = CGAffineTransform(rotationAngle: -(CGFloat)(M_PI_2))
+        
+        var temp: CGRect = volumeSlider.frame
+        temp.origin.x = volumeButton.frame.origin.x
+        temp.origin.y = volumeButton.frame.origin.y - volumeSlider.frame.height
+        
+        volumeSlider.frame = temp
+        
     }
 
+    @IBAction func songTimeBtnPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func playPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func stopPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func nxtPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func backPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func volumePressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func shufflePressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func repeatPressed(_ sender: UIButton) {
+    }
 
+    @IBAction func timeSliderChanged(_ sender: UISlider) {
+    }
+    @IBAction func volumeSliderChanged(_ sender: UISlider) {
+    }
+    
+    
 }
 
