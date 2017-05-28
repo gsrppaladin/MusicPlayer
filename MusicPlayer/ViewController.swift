3fa2e4aa8e4d129f5112630b894ce9252c071e7f
Sam Greenhill <samgreenhill@Sams-MBP.home>
2017-05-28 18:09:34 -0400

    worked on the different buttons for the music player.

--- MusicPlayer/ViewController.swift
+++ MusicPlayer/ViewController.swift
@@ -49,6 +49,12 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
         }
         timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateSlider), userInfo: nil, repeats: true)
         
+        volumeSlider.alpha = 0
+        //this hides the volumeSlider now we jsut have to change the alpha when we press the volume button.
+        //can say is hidden but with this it is easier to control.
+        
+        
+        
     }
 
     
@@ -145,29 +151,116 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
     }
     
     @IBAction func playPressed(_ sender: UIButton) {
+        if musicFiles.count > 0 {
+            if !musicPlayer.isPlaying {
+                musicPlayer.play()
+                sender.setImage(UIImage(named: "pause"), for: .normal)
+                
+            } else {
+                musicPlayer.pause()
+                sender.setImage(UIImage(named: "play"), for: .normal)
+            }
+        }
     }
     
     @IBAction func stopPressed(_ sender: UIButton) {
+        if musicPlayer.isPlaying {
+            musicPlayer.stop()
+            //songNameLbl.text = ""
+            musicPlayer.currentTime = 0
+        }
+        
     }
     
     @IBAction func nxtPressed(_ sender: UIButton) {
+        if musicFiles.count > 0 {
+            if !shuffleOn {
+                currentIndex += 1
+                if currentIndex == musicFiles.count - 1 {
+                    currentIndex = 0
+                }
+            } else {
+                //if shuffle is on then we want to set a random index.
+                currentIndex = getRandomSongIndex()
+            }
+            playMusic()
+        }
+        
+    }
+    
+    func getRandomSongIndex() -> Int {
+        let index:Int = Int(arc4random_uniform(UInt32(musicFiles.count)))
+        return index
+        
     }
     
     @IBAction func backPressed(_ sender: UIButton) {
+        
+        if musicFiles.count > 0 {
+            if !shuffleOn {
+                currentIndex -= 1
+                if currentIndex < 0 {
+                   currentIndex = musicFiles.count - 1
+                }
+            
+            } else {
+                currentIndex = getRandomSongIndex()
+            }
+            playMusic()
+        }
     }
     
     @IBAction func volumePressed(_ sender: UIButton) {
+        if !volumeShowing {
+            UIView.animate(withDuration: 0.1, delay: 0, options: .curveEaseOut, animations: {
+                self.volumeSlider.alpha = 1
+            }, completion: nil)
+        } else {
+            UIView.animate(withDuration: 0.1, delay: 0, options: .curveEaseOut, animations: {
+                self.volumeSlider.alpha = 0
+            }, completion: nil)
+        }
+        
+        volumeShowing = !volumeShowing
+        //in the end need to change state of volumeshowing state so that next time the volume is pressed it does the opposite it has done before.
+        
     }
     
     @IBAction func shufflePressed(_ sender: UIButton) {
+        if shuffleOn {
+            sender.setImage(UIImage(named: "shuffle_inactive"), for: .normal)
+        } else {
+            sender.setImage(UIImage(named: "shuffle_active"), for: .normal)
+        }
+        shuffleOn = !shuffleOn
     }
     
     @IBAction func repeatPressed(_ sender: UIButton) {
+        if repeatOn {
+            sender.setImage(UIImage(named: "repeat_inactive"), for: .normal)
+        } else {
+            sender.setImage(UIImage(named: "repeat_active"), for: .normal)
+        }
+        repeatOn = !repeatOn
+        
     }
 
     @IBAction func timeSliderChanged(_ sender: UISlider) {
+        if musicPlayer.isPlaying {
+            musicPlayer.currentTime = TimeInterval(timeSlider.value)
+        }
     }
+    
+    
     @IBAction func volumeSliderChanged(_ sender: UISlider) {
+        if sender.value == 0 {
+            volumeButton.setImage(UIImage(named: "volume_inactive"), for: .normal)
+        } else {
+            volumeButton.setImage(UIImage(named: "volume_active"), for: .normal)
+        }
+        
+        musicPlayer.volume = volumeSlider.value
+        
     }
     
     
